236aeefdc78c168370a0516aad1f47274016a187
vgupta1 <guptavis@usc.edu>
2025-02-01 19:45:42 -0800

    renaming package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="313_tools",
+    name="buad313_tools",
     version="0.1",
     packages=find_packages(),
     install_requires=[],  # Add dependencies here if needed
